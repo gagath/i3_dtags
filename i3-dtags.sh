73214bf8b622153d0205fe983f57c5348dcdcb6d
Romain Porte <microjoe@mailoo.org>
2014-09-02 23:15:33 +0200

    Handling no arguments with default font

--- i3-dtags.sh
+++ i3-dtags.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-i3-msg workspace $(i3-msg -t get_workspaces | i3-dtags | dmenu -fn "$1")
+if [ ! -z $1 ]; then
+    # Use custom font if provided
+    i3-msg workspace $(i3-msg -t get_workspaces | i3-dtags | dmenu -fn "$1")
+else
+    i3-msg workspace $(i3-msg -t get_workspaces | i3-dtags | dmenu)
+fi
+
